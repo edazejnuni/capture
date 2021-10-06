1ad64139d189c011d8b6d86c1cef64b7b4cabf5e
edazejnuni <edona.zejnuni@gmail.com>
2021-10-06 12:16:31 +0200

    4. Faq Section

---
+++ src/components/FaqSection.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+const FaqSection = () => {
+    return (
+        <div className="faq">
+            <h2>Any Questions <span>FAQ</span></h2>
+            <div className="questions">
+                <h4>How Do I Start?</h4>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id, magni!</p>
+                </div>
+            </div>
+            <div className="questions">
+                <h4>Daily Schedule</h4>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id, magni!</p>
+                </div>
+            </div>
+            <div className="questions">
+                <h4>Different Payment Methods</h4>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id, magni!</p>
+                </div>
+            </div>
+            <div className="questions">
+                <h4>What products do you offer.</h4>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id, magni!</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default FaqSection;
