6c0a6abd6e4d44cb2b684744e73478f31a46dfc6
edazejnuni <edona.zejnuni@gmail.com>
2021-10-07 23:10:17 +0200

    8. Our Work

--- src/App.js
+++ src/App.js
@@ -3,14 +3,29 @@ import React from "react";
 import GlobalStyle from "./components/GlobalStyle";
 //import pages
 import AboutUs from "./pages/AboutUs";
+import ContactUS from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
 import Nav from "./components/Nav";
+//Router
+import {Switch, Route} from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <GlobalStyle/>
         <Nav/>
-        <AboutUs/>
+        <Switch>
+          <Route path="/" exact>
+            <AboutUs/>
+          </Route>
+          <Route path="/work">
+            <OurWork/>
+          </Route>
+          <Route path="/contact">
+            <ContactUS/>
+          </Route>
+        </Switch>
       </header>
     </div>
   );
